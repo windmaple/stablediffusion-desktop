277670243e9b9ae0aa746358f94cb4c2a940b9ce
Wayne Wei <windmaple@gmail.com>
2022-08-27 10:48:52 +0800

    Add mac arm64 build script

---
+++ make_builds_mac_arm64.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pyinstaller SD-desktop.py -w --noconfirm --onefile --icon ./assets/stock.png \
+                          --target-architecture arm64 --add-data "assets:assets" --hidden-import=pytorch \
+                          --hidden-import=huggingface_hub.repository  \
+                          --copy-metadata torch --copy-metadata tqdm --copy-metadata regex \
+                          --copy-metadata requests --copy-metadata packaging --copy-metadata filelock \
+                          --copy-metadata tokenizers --copy-metadata importlib_metadata --copy-metadata numpy
